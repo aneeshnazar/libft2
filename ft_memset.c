3b369014ac76293c0f7863dafa97a8ecc66ecd48
aneeshnazar <aneesh_naz@yahoo.com>
2018-01-07 22:24:17 -0800

    memset test

--- ft_memset.c
+++ ft_memset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anazar <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: anazar <anazar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 09:54:28 by anazar            #+#    #+#             */
-/*   Updated: 2017/07/08 16:24:28 by anazar           ###   ########.fr       */
+/*   Updated: 2018/01/07 22:23:47 by anazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 void				*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*str;
+	size_t			*str;
 	size_t			i;
+	size_t			len_mod;
+	size_t			arr[5];
 
 	i = 0;
+	len_mod = len / 4;
 	str = b;
-	while (i < len)
+	arr[0] = c + (c << 8) + (c << 16) + (c << 24);
+	arr[1] = arr[0] % 0x100;
+	arr[2] = arr[0] % 0x10000;
+	arr[3] = arr[0] % 0x1000000;
+	while (i < len_mod)
 	{
-		str[i] = c;
+		str[i] = arr[0];
 		++i;
 	}
+	if (len % 4)
+		str[i] = arr[len % 4];
 	return (b);
 }
