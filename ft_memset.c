a0f4c31d690c403272e8e9272e674c31cc3b1a55
aneeshnazar <aneesh_naz@yahoo.com>
2018-01-07 22:27:40 -0800

    memset test failed

--- ft_memset.c
+++ ft_memset.c
@@ -3,35 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anazar <anazar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anazar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 09:54:28 by anazar            #+#    #+#             */
-/*   Updated: 2018/01/07 22:23:47 by anazar           ###   ########.fr       */
+/*   Updated: 2017/07/08 16:24:28 by anazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void				*ft_memset(void *b, int c, size_t len)
 {
-	size_t			*str;
+	unsigned char	*str;
 	size_t			i;
-	size_t			len_mod;
-	size_t			arr[5];
 
 	i = 0;
-	len_mod = len / 4;
 	str = b;
-	arr[0] = c + (c << 8) + (c << 16) + (c << 24);
-	arr[1] = arr[0] % 0x100;
-	arr[2] = arr[0] % 0x10000;
-	arr[3] = arr[0] % 0x1000000;
-	while (i < len_mod)
+	while (i < len)
 	{
-		str[i] = arr[0];
+		str[i] = c;
 		++i;
 	}
-	if (len % 4)
-		str[i] = arr[len % 4];
 	return (b);
 }
